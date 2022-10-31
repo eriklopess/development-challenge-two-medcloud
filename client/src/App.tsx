f8b36c171327a6447c9cb979411421486d60d162
Erik Lopes <erik.lopesoliveira2019@gmail.com>
2022-10-31 13:58:36 -0300

    style: instalando eslint e colocando suas normas em arquivos existentes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,19 @@
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/"  />
+        <Route path="/" />
         <Route path="/register" />
         <Route path="/login" />
         <Route path="/customers" />
         <Route path="/customers/:id" />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
